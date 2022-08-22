b96d2e8ae074793de60951f96bd55e579812fac4
Kiptanui Boaz <boaserem022@gmail.com>
2022-08-22 18:48:31 +0300

    Update README.jsx

--- README.jsx
+++ README.jsx
@@ -1,12 +1,12 @@
-- 👋 Hi, I’m @kiptanuiBoaz
-- 👀 I’m interested in Web Developement (MERN stack)
-- 🌱 I’m currently learning Web 3 (DAPPs)
-- 💞️ I’m looking to collaborate on web apps and open source projects
-- 📫 How to reach me
-Twitter @CleverFreak254
-Gmail boaserem022@gmail.com
+import React from "react";
+import "./footer.css"
 
-<!---
-kiptanuiBoaz/kiptanuiBoaz is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+function Footer(){
+    return(
+        <div className="footer">
+            <p>Copyright ©{new Date().getFullYear()} || Developed by Kiptanui Boaz </p>
+        </div>
+    )
+}
+
+export default Footer;
